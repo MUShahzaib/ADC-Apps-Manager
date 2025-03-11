96c7f99dd2c5f18b1aefd190666db4911c5c55ba
MUShahzaib <mshahzaib6699@gmail.com>
2025-03-11 16:48:28 +0500

    refactor: design updated to tabs view feat: package name, playstore link, appstore link and creatives link added

--- script.js
+++ script.js
@@ -5,18 +5,33 @@ const updateAppName = document.getElementById('updateAppName');
 const updatePackageName = document.getElementById('updatePackageName');
 const updateShowAds = document.getElementById('updateShowAds');
 const updateShowTestAds = document.getElementById('updateShowTestAds');
+const updatePlayStoreLink = document.getElementById('updatePlayStoreLink');
+const updateAppStoreLink = document.getElementById('updateAppStoreLink');
+const updateCreativesLink = document.getElementById('updateCreativesLink');
 const appsTable = document.getElementById('appsTable').getElementsByTagName('tbody')[0];
 const searchInput = document.getElementById('search');
 
 //const scriptUrl = 'https://script.google.com/macros/s/AKfycbxNsI-kTPU80ikKg10S85EEvUNeZ4kmhrZbdwJ1iVG3hEJIIRWpkmT3ZnEbWzP4vAyx/exec';
-const scriptUrl='https://script.google.com/macros/s/AKfycby-1MlpudFRUELDjzOlXv4y_TTw4dYsqbzly2MtTQaWOgkDVLTZKEnbwWsxLinGeTcOKg/exec';
+const scriptUrl='https://script.google.com/macros/s/AKfycbwO5GCX9kINLt3XN4oKTv5mnLeG2Rw0XATL3SvDituDgGZlEYgUk3iiKx6RCV8qNvPm3Q/exec';
 
 document.addEventListener('DOMContentLoaded',async ()=>{
     await fetchApps();
     appId.focus();
 });
 
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
 
+    const buttons = document.querySelectorAll('.tab-button');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+    document.querySelector(`.tab-button[onclick="showTab('${tabId}')"]`).classList.add('active');
+}
 
 async function fetchApps() {
     showPopup("Loading apps...");
@@ -31,21 +46,37 @@ function loadAppsInTable(data){
     data.forEach((row, index) => {
       if (index === 0) return; // Skip header
 
+      const appId=row[0];
+      const appName=row[1];
       // Convert boolean values to "Yes" or "No"
       const showAds = row[2] === "true" || row[2] === true ? "Yes" : "No";
       const showTestAds = row[3] === "true" || row[3] === true ? "Yes" : "No";
+      const packageName=row[4];
+      const playStoreLink= row[5];
+      const appStoreLink=row[6];
+      const creativesLink=row[7];
+
+      const showAdsValue= row[2];
+      const showTestAdsValue=row[3];
+      //console.log("s",showAdsValue);
+     const appInfo={
+        appId,appName,packageName,showAdsValue,showTestAdsValue,playStoreLink,appStoreLink,creativesLink
+      }
 
       const tr = document.createElement("tr");
       tr.innerHTML = `
-            <td>${row[0]}</td>
-            <td>${row[4]}</td>
-            <td>${row[1]}</td>
+            <td>${appId}</td>
+            <td>${packageName}</td>
+            <td>${appName}</td>
             <td>${showAds}</td>
             <td>${showTestAds}</td>
-            <td>
-             <button class='update-app-btn' onclick="openUpdatePopup('${row[0]}', '${row[1]}', ${row[2]}, ${row[3]},'${row[4]}')">
-               <i class="fas fa-edit"></i>
-             </button>
+            <td><a href='${playStoreLink}' target='_blank'>Store link</a></td>
+            <td><a href='${appStoreLink}' target='_blank'>AppStore link</a></td>
+            <td><a href='${creativesLink}' target='_blank'>Creatives</a></td>
+            <td class="checkbox-group">
+            <button class='update-app-btn' onclick="openUpdatePopup('${JSON.stringify(appInfo).replace(/'/g, "\\'").replace(/"/g, '&quot;')}')">
+            <i class="fas fa-edit"></i>
+            </button></br>
              <button class='delete-app-btn' onclick="deleteApp('${row[0]}')">
               <i class="fas fa-trash-alt"></i>
              </button>
@@ -61,6 +92,10 @@ function addApp() {
     const packageName = document.getElementById('packageName').value;
     const showAds = document.getElementById('showAds').checked;
     const showTestAds = document.getElementById('showTestAds').checked;
+    const playStoreLink= document.getElementById('playStoreLink').value;
+    const appStoreLink= document.getElementById('appStoreLink').value;
+    const creativesLink= document.getElementById('creativesLink').value;
+
 
    showPopup("Adding new app...");
     fetch(scriptUrl, {
@@ -71,7 +106,10 @@ function addApp() {
             appName,
             showAds,
             showTestAds,
-            packageName
+            packageName,
+            playStoreLink,
+            appStoreLink,
+            creativesLink
         })
     }).then(async () => {
         await fetchApps();
@@ -93,18 +131,24 @@ function deleteApp(appId) {
     });
 }
 
-function openUpdatePopup(appId, appName, showAds, showTestAds,packageName) {
-    updateAppId.value = appId;
-    updateAppName.value = appName;
-    updatePackageName.value=packageName;
-    updateShowAds.checked = showAds;
-    updateShowTestAds.checked = showTestAds;
+function openUpdatePopup(appInfoJson) {
+   // console.log('here'+appInfoJson.showTestAdsValue);
+    const appInfo = JSON.parse(appInfoJson);
+    updateAppId.value = appInfo.appId;
+    updateAppName.value = appInfo.appName;
+    updatePackageName.value=appInfo.packageName;
+    updateShowAds.checked = appInfo.showAdsValue;
+    updateShowTestAds.checked = appInfo.showTestAdsValue;
+    updatePlayStoreLink.value=appInfo.playStoreLink;
+    updateAppStoreLink.value=appInfo.appStoreLink;
+    updateCreativesLink.value=appInfo.creativesLink;
     updatePopup.style.display = 'flex';
     updateAppName.focus();
 }
 
-function closePopup() {
+ function closePopup() {
     updatePopup.style.display = 'none';
+    //await fetchApps();
 }
 
 function updateApp() {
@@ -113,6 +157,11 @@ function updateApp() {
     const showAds = updateShowAds.checked;
     const showTestAds = updateShowTestAds.checked;
     const packageName= updatePackageName.value;
+    const playStoreLink= updatePlayStoreLink.value;
+    const appStoreLink=updateAppStoreLink.value;
+    const creativesLink=updateCreativesLink.value;
+
+   // console.log('playstore link',playStoreLink);
 
     showPopup("Updating app...");
     fetch(scriptUrl, {
@@ -123,12 +172,15 @@ function updateApp() {
             appName,
             showAds,
             showTestAds,
-            packageName
+            packageName,
+            playStoreLink,
+            appStoreLink,
+            creativesLink
         })
     }).then(async () => {
-        await fetchApps();
         closePopup();
-        hidePopup();
+        await fetchApps();
+       // hidePopup();
     });
 }
 
